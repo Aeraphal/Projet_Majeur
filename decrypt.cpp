39633d6101a6a937bbe80c61e5228994ddc2a8b0
Aeraphal <alexandre.jolin@cpe.fr>
2023-06-02 16:12:33 +0200

    Init

---
+++ decrypt.cpp
@@ -0,0 +1,20 @@
+
+
+
+
+
+
+
+enum Caracter {esp = 0, a = 1, b = 5, c = 3, d = 11, e = 9, f = 7, g = 15, 
+                h = 13, i = 6, j = 14, k = 17, l = 21, m = 19, n = 27,
+                o = 25, p = 23, q = 31, r = 29, s = 22, t = 30, u = 49,
+                v = 53, w = 46, x = 51, y = 59, z = 57, c_ced = 55,
+                e_ai = 63, e_gr = 54, u_gr = 62, un = 33, deux = 37, 
+                trois = 35, quatre = 43, cinq = 41, six = 39, sept = 47, 
+                huit = , neuf = , zero = , virg = , pts_virg = , deux_pts = ,
+                pts = , pts_int = ,pts_exc = , trem = , par_dr = ,
+                par_gau = 52, ast = 24, apost = 16, tir = 48, Ital = ,
+                Maj = 34, Num = 32};
+Caracter x = a;
+
+
