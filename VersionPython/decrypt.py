9cf65501d38a1d0970d274b931766b7cee1dc400
Aeraphal <alexandre.jolin@cpe.fr>
2023-06-05 16:21:48 +0200

    truc chouette

---
+++ VersionPython/decrypt.py
@@ -0,0 +1,13 @@
+import cv2
+
+
+#	Chaque lettre est individualisée dans sa case de grillage. 
+#	On définit les position 1, 2, 3, 4, 5 et 6 qui correspondent à des cases pleines ou vides. (2^6 = 64 possibilités)
+#	On effectue un traitement binaire avec ces informations. 
+#Chaque résultat du traitement binaire renvoie un caractère bien précis.
+#		Les résultats vident renvoie un espace.
+#On stocke le résultat du traitement binaire dans une file. 
+
+
+
+
