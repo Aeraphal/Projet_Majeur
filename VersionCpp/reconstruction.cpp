c49125bbb9cfe4ef02adaa8bc888bd5a337f37f9
Aeraphal <alexandre.jolin@cpe.fr>
2023-06-06 10:01:21 +0200

    Suppression des dossiers "Versions"

--- VersionCpp/reconstruction.cpp
+++
@@ -1,7 +0,0 @@
-//Reconstruction du texte entier
-//	Nous avons l’ensemble des éléments de la file. 
-//	On déroule le fil afin de recréer le texte.
-//		On observe la succession de deux éléments pour les associer au cas ou.
-//			, suivi d’une lettre la met en majuscule. 
-//			4 suivant une lettre pose un point.
-//			Plusieurs espaces successifs renvoient un seul espace.
