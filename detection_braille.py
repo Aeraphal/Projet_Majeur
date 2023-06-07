7f3b6dd3ae7c821b654c3e44a0d25163da3e5b1f
Lecoquee <91311343+Lecoquee@users.noreply.github.com>
2023-06-07 17:14:02 +0200

    Add files via upload

--- detection_braille.py
+++ detection_braille.py
@@ -80,28 +80,75 @@ repere_longueur.sort()
 
 # Longueurs classées par ligne
 repere_longueur_final = [[] for x in range(len(repere_hauteur_final))]
+repere_hauteur_final_2 = [[] for x in range(len(repere_hauteur_final))]
 for i in range(len(circles)):
     for j in range(len(repere_hauteur_final)):
         for k in range(len(repere_longueur)):
             if circles[i,0] == repere_longueur[k] and circles[i,1] in repere_hauteur_final[j]:
                 repere_longueur_final[j].append(repere_longueur[k])
+                repere_hauteur_final_2[j].append(circles[i,1])
 
+
+min = 0
+
+
+def tri_insertion(La,Ha):
+    L = La
+    H = Ha
+    N = len(L)
+    for n in range(1,N):
+        cle = L[n]
+        j = n-1
+        while j>=0 and L[j] > cle:
+            L[j+1], L[j] = L[j], L[j+1] # decalage
+            H[j+1], H[j] = H[j], H[j+1]
+            j = j-1
+    return(L,H)
+
+truc1 = []
+truc2 = []
 for i in range(len(repere_longueur_final)):
-    repere_longueur_final[i].sort()
-print(repere_longueur_final)
+    tri_insertion(repere_longueur_final[i],repere_hauteur_final_2[i])
+
+
 
 
 # Calcul des distances entre deux cercles
 espace1 = repere_longueur[-1]
 espace2 = repere_longueur[-2]
 distance = 0
-for i in range(len(repere_longueur)-2):
-    distance = repere_longueur[i+1]-repere_longueur[i]
-    if espace1 > distance:
-        espace1 = distance
-    if abs(espace1 - distance) >= 2:
-        if espace2 > distance:
+for i in range(len(repere_longueur_final[1])-2):
+    distance = repere_longueur_final[1][i+1]-repere_longueur_final[1][i]
+    if distance > 2:
+        if espace1 >= distance :                  # distance < espace1 < distance + 2
+            espace1 = distance
+        elif espace2 > distance :
             espace2 = distance
+espace_final = (espace1 + espace2) / 2
+
+print("espace 1 = ", espace1, ", l'espace 2 = ", espace2, " et l'espace final = ", espace_final)
+
+list_caractere = [[],[]]
+caractere = []
+for i in range(len(repere_hauteur_final_2)):
+    caractere = [[repere_longueur_final[i][0], repere_hauteur_final_2[i][0]]]
+    for k in range(len(repere_hauteur_final_2[i])-1):
+        print("barbare = ", repere_longueur_final[i][k+1] - repere_longueur_final[i][k], "caractere actuel = ", caractere)
+        if repere_longueur_final[i][k+1] - repere_longueur_final[i][k] < espace_final:
+            caractere.append([repere_longueur_final[i][k+1],repere_hauteur_final_2[i][k+1]])
+        elif repere_longueur_final[i][k+1] - repere_longueur_final[i][k] > espace_final*2: 
+            list_caractere[i].append(caractere)
+            caractere = [[repere_longueur_final[i][k+1],repere_hauteur_final_2[i][k+1]]]
+            list_caractere[i].append([])
+        else : 
+            list_caractere[i].append(caractere)
+            caractere = [[repere_longueur_final[i][k+1],repere_hauteur_final_2[i][k+1]]]
+    list_caractere[i].append(caractere)
+
+print("la liste de caractère cripté donne : ", list_caractere, "ayant ", len(list_caractere), " composant.")
+print("la liste en longueur trié donne : ",repere_longueur_final)
+print("la liste trié donne : ", repere_hauteur_final_2)
+
 
 
 cv.namedWindow('detected circles', cv.WINDOW_NORMAL)
